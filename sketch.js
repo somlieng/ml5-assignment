0964814547e3f55b959b418c2199dd2ca315ab90
Som Euakarn Liengtiraphan <eliengti@u.rochester.edu>
2020-11-02 19:28:31 -0500

    image classier didn't work, doing word2vec now

--- sketch.js
+++ sketch.js
@@ -1,39 +1,74 @@
-//DO NOT SUBMIT THIS CODE THIS IS A STRAIGHT COPY
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
 
-let classifier;
-let imageResult;
+/* ===
+ml5 Example
+Word2Vec example with p5.js. Using a pre-trained model on common English words.
+=== */
 
-let videoBox;
+let word2Vec;
 
-//Trained fruits and vegetable model
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/uowWv7iP7/';
+function modelLoaded() {
+  select('#status').html('Model Loaded');
+}
 
 function setup() {
-    noCanvas();
-  // Create a camera input
-    videoBox = createCapture(VIDEO);
-    var videoDiv = document.getElementById("video");
-    videoDiv.appendChild(videoBox);
-  // Initialize the Image Classifier method with MobileNet and the video as the second argument
-//  classifier = ml5.imageClassifier(imageModelURL + 'model.json', video, modelReady);
-    classifier = ml5.imageClassifier('MobileNet', video, modelReady);
-    imageResult = createP('Loading model and video...');
-    document.getElementById("results").appendChild(imageResult);  
-}
+  noLoop();
+  noCanvas();
 
-function modelReady() {
-  console.log('Model Ready');
-  classifyVideo();
-}
+  // Create the Word2Vec model with pre-trained file of 5000 words
+  word2Vec = ml5.word2vec('wordvecs10000.json', modelLoaded);
 
-// Get a prediction for the current video frame
-function classifyVideo() {
-  classifier.classify(gotResult);
-}
+  // Select all the DOM elements
+  let nearWordInput = select('#nearword');
+  let nearButton = select('#submit');
+  let nearResults = select('#results');
 
-// When we get a result
-function gotResult(err, results) {
-  // The results are in an array ordered by confidence.
-  imageResult.html(results[0].label + ' ' + nf(imageResult[0].confidence, 0, 2));
-  classifyVideo();
+  let betweenWordInput1 = select("#between1");
+  let betweenWordInput2 = select("#between2");
+  let betweenButton = select("#submit2");
+  let betweenResults = select("#results2");
+
+  let addInput1 = select("#isto1");
+  let addInput2 = select("#isto2");
+  let addInput3 = select("#isto3");
+  let addButton = select("#submit3");
+  let addResults = select("#results3");
+
+  // Finding the nearest words
+  nearButton.mousePressed(() => {
+    let word = nearWordInput.value();
+    word2Vec.nearest(word, (err, result) => {
+      let output = '';
+      if (result) {
+        for (let i = 0; i < result.length; i++) {
+          output += result[i].word + '<br/>';
+        }
+      } else {
+        output = 'No word vector found';
+      }
+      nearResults.html(output);
+    });
+  });
+
+  // Finding the average of two words
+  betweenButton.mousePressed(() => {
+    let word1 = betweenWordInput1.value();
+    let word2 = betweenWordInput2.value();
+    word2Vec.average([word1, word2], 4, (err, average) => {
+      betweenResults.html(average[0].word);
+    })
+  });
+
+  // Adding two words together to "solve" an analogy
+  addButton.mousePressed(() => {
+    let is1 = addInput1.value();
+    let to1 = addInput2.value();
+    let is2 = addInput3.value();
+    word2Vec.subtract([to1, is1])
+      .then(difference => word2Vec.add([is2, difference[0].word]))
+      .then(result => addResults.html(result[0].word))
+  });
 }
