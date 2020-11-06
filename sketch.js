0c74b105f3bac5e6664932d252b71e82dcc554f7
Som Euakarn Liengtiraphan <eliengti@u.rochester.edu>
2020-11-06 02:30:03 -0500

    Added in more description

--- sketch.js
+++ sketch.js
@@ -196,7 +196,7 @@ let verseShowing = false;
 let inputShowing = false;
 
 function modelLoaded() {
-  select('#status').html('Model Loaded');
+  select('#status').html('Computer ears now listening!');
 }
 
 function preload(){
@@ -263,7 +263,7 @@ function showMoreInputs(button){
 
 async function bohemianRhapsody(){
     
-    initializeDOM();
+        initializeDOM();
     
     calcWord(adj1_value,adjective1Result);
     adj1_list = await listWord(adj1_value);
@@ -272,129 +272,76 @@ async function bohemianRhapsody(){
     calcWord(disaster_value,disasterResult);
     disaster_list = await listWord(disaster_value);
     calcWord(verb1_value,verb1Result);
-//    verb1_list = await listWord(verb1_value);
+    verb1_list = await listWord(verb1_value);
     calcWord(noun2_value,noun2Result);
-//    noun2_list = await listWord(noun2_value);
+    noun2_list = await listWord(noun2_value);
     calcWord(organ_value,organResult);
-//    organ_list = await listWord(organ_value);
+    organ_list = await listWord(organ_value);
     calcWord(adj2_value,adjective2Result);
-//    adj2_list = await listWord(adj2_value);
+    adj2_list = await listWord(adj2_value);
     calcWord(noun3_value,noun3Result);
-//    noun3_list = await listWord(noun3_value);
+    noun3_list = await listWord(noun3_value);
     calcWord(noun4_value,noun4Result);
-//    noun4_list = await listWord(noun4_value);
+    noun4_list = await listWord(noun4_value);
     
     calcWord(familyMember_value,familyMember1Result);
     calcWord(familyMember_value,familyMember2Result);
     calcWord(familyMember_value,familyMember3Result);
+    family_list = await listWord(familyMember_value);
     calcWord(noun5_value,noun5Result);
+    noun5_list = await listWord(noun5_value);
     calcWord(bodypart_value,bodypartResult);
+    bodypart_list = await listWord(bodypart_value);
     calcWord(verb2_value,verb2Result);
+    verb2_list = await listWord(verb2_value);
     calcWord(carry_value,carryResult1);
     calcWord(carry_value,carryResult2);
+    carry_list = await listWord(carry_value);
     
     calcWord(verb3_value,verb3Result);
+    verb3_list = await listWord(verb3_value);
     calcWord(bodypart2_value,bodypart2Result);
+    bodypart2_list = await listWord(bodypart2_value);
     calcWord(verb4_value,verb4Result);
+    verb4_list = await listWord(verb4_value);
     calcWord(noun6_value,noun6Result);
+    noun6_list = await listWord(noun6_value);
     calcWord(verb5_value,verb5Result);
+    verb5_list = await listWord(verb5_value);
     
     calcWord(adj3_value,adjective3Result);
+    adj3_list = await listWord(adj3_value);
     calcWord(noun7_value,noun7Result);
+    noun7_list = await listWord(noun7_value);
     calcWord(nature1_value,nature1Result);
+    nature1_list = await listWord(nature1_value);
     calcWord(nature2_value,nature2Result);
+    nature2_list = await listWord(nature2_value);
     calcWord(human_value,humanResult);
+    human_list = await listWord(human_value);
     calcWord(verb6_value,verb6Result);
+    verb6_list = await listWord(verb6_value);
     calcWord(verb7_value,verb7Result);
+    verb7_list = await listWord(verb7_value);
     calcWord(noun8_value,noun8Result);
+    noun8_list = await listWord(noun8_value);
     
     calcWord(verb8_value,verb8Result);
+    verb8_list = await listWord(verb8_value);
     calcWord(verb9_value,verb9Result);
+    verb9_list = await listWord(verb9_value);
     calcWord(verb10_value,verb10Result);
+    verb10_list = await listWord(verb10_value);
     calcWord(verb11_value,verb11Result);
+    verb11_list = await listWord(verb11_value);
     
     calcWord(noun9_value,noun9Result);
+    noun9_list = await listWord(noun9_value);
     calcWord(verb12_value,verb12Result);
+    verb12_list = await listWord(verb12_value);
     
     showLyrics();
     tooltip();
-    
-//        initializeDOM();
-//    
-//    calcWord(adj1_value,adjective1Result);
-//    adj1_list = await listWord(adj1_value);
-//    calcWord(noun1_value,noun1Result);
-//    noun1_list = await listWord(noun1_value);
-//    calcWord(disaster_value,disasterResult);
-//    disaster_list = await listWord(disaster_value);
-//    calcWord(verb1_value,verb1Result);
-//    verb1_list = await listWord(verb1_value);
-//    calcWord(noun2_value,noun2Result);
-////    noun2_list = await listWord(noun2_value);
-//    calcWord(organ_value,organResult);
-////    organ_list = await listWord(organ_value);
-//    calcWord(adj2_value,adjective2Result);
-////    adj2_list = await listWord(adj2_value);
-//    calcWord(noun3_value,noun3Result);
-////    noun3_list = await listWord(noun3_value);
-//    calcWord(noun4_value,noun4Result);
-////    noun4_list = await listWord(noun4_value);
-//    
-//    calcWord(familyMember_value,familyMember1Result);
-//    calcWord(familyMember_value,familyMember2Result);
-//    calcWord(familyMember_value,familyMember3Result);
-////    family_list = await listWord(familyMember_value);
-//    calcWord(noun5_value,noun5Result);
-////    noun5_list = await listWord(noun5_value);
-//    calcWord(bodypart_value,bodypartResult);
-////    bodypart_list = await listWord(bodypart_value);
-//    calcWord(verb2_value,verb2Result);
-////    verb2_list = await listWord(verb2_value);
-//    calcWord(carry_value,carryResult1);
-//    calcWord(carry_value,carryResult2);
-////    carry_list = await listWord(carry_value);
-//    
-//    calcWord(verb3_value,verb3Result);
-////    verb3_list = await listWord(verb3_value);
-//    calcWord(bodypart2_value,bodypart2Result);
-////    bodypart2_list = await listWord(bodypart2_value);
-//    calcWord(verb4_value,verb4Result);
-////    verb4_list = await listWord(verb4_value);
-//    calcWord(noun6_value,noun6Result);
-////    noun6_list = await listWord(noun6_value);
-//    calcWord(verb5_value,verb5Result);
-////    verb5_list = await listWord(verb5_value);
-//    
-//    calcWord(adj3_value,adjective3Result);
-////    adj3_list = await listWord(adj3_value);
-//    calcWord(noun7_value,noun7Result);
-////    noun7_list = await listWord(noun7_value);
-//    calcWord(nature1_value,nature1Result);
-////    nature1_list = await listWord(nature1_value);
-//    calcWord(nature2_value,nature2Result);
-////    nature2_list = await listWord(nature2_value);
-//    calcWord(human_value,humanResult);
-////    human_list = await listWord(human_value);
-//    calcWord(verb6_value,verb6Result);
-////    verb6_list = await listWord(verb6_value);
-//    calcWord(verb7_value,verb7Result);
-////    verb7_list = await listWord(verb7_value);
-//    calcWord(noun8_value,noun8Result);
-////    noun8_list = await listWord(noun8_value);
-//    
-//    calcWord(verb8_value,verb8Result);
-////    verb8_list = await listWord(verb8_value);
-//    calcWord(verb9_value,verb9Result);
-////    verb9_list = await listWord(verb9_value);
-//    calcWord(verb10_value,verb10Result);
-////    verb10_list = await listWord(verb10_value);
-//    calcWord(verb11_value,verb11Result);
-////    verb11_list = await listWord(verb11_value);
-//    
-//    calcWord(noun9_value,noun9Result);
-////    noun9_list = await listWord(noun9_value);
-//    calcWord(verb12_value,verb12Result);
-////    verb12_list = await listWord(verb12_value);
     
 }
 
@@ -409,10 +356,38 @@ function tooltip(){
     tippyHelper('#adj2',adj2_list);
     tippyHelper('#noun3',noun3_list);
     tippyHelper('#noun4',noun4_list);
-    tippyHelper('#family1',familyMember_value);
-    tippyHelper('#family2',familyMember_value);
-    tippyHelper('#family3',familyMember_value);
-    //tippyHelper('#',);
+    
+    tippyHelper('#family1',family_list);
+    tippyHelper('#family2',family_list);
+    tippyHelper('#family3',family_list);
+    tippyHelper('#noun5',noun5_list);
+    tippyHelper('#bodypart',bodypart_list);
+    tippyHelper('#verb2',verb2_list);
+    tippyHelper('#carry1',carry_list);
+    tippyHelper('#carry2',carry_list);
+    
+    tippyHelper('#verb3',verb3_list);
+    tippyHelper('#bodypart2',bodypart2_list);
+    tippyHelper('#verb4',verb4_list);
+    tippyHelper('#noun6',noun6_list);
+    tippyHelper('#verb5',verb5_list);
+    
+    tippyHelper('#adj3',adj3_list);
+    tippyHelper('#noun7',noun7_list);
+    tippyHelper('#nature1',nature1_list);
+    tippyHelper('#nature2',nature2_list);
+    tippyHelper('#human',human_list);
+    tippyHelper('#verb6',verb6_list);
+    tippyHelper('#verb7',verb7_list);
+    tippyHelper('#noun8',noun8_list);
+    
+    tippyHelper('#verb8',verb8_list);
+    tippyHelper('#verb9',verb9_list);
+    tippyHelper('#verb10',verb10_list);
+    tippyHelper('#verb11',verb11_list);
+    
+    tippyHelper('#noun9',noun9_list);
+    tippyHelper('#verb12',verb12_list);
 }
 
 function tippyHelper(hover,word){
@@ -431,137 +406,170 @@ function initializeDOM(){
     adjective1input = select('#real');
     adj1_value = adjective1input.value();
     adjective1Result = select('#adj1');
+    adj1_list = '';
     
     noun1input = select('#life');
     noun1_value = noun1input.value();
     noun1Result = select('#noun1');
+    noun1_list = '';
     
     disasterinput = select('#landslide');
     disaster_value = disasterinput.value();
     disasterResult = select('#natrual-disaster');
+    disaster_list = '';
     
     verb1input = select('#escape');
     verb1_value = verb1input.value();
     verb1Result = select('#verb1');
+    verb1_list = '';
     
     noun2input = select('#reality');
     noun2_value = noun2input.value();
     noun2Result = select('#noun2');
+    noun2_list = '';
     
     organinput = select('#eyes');
     organ_value = organinput.value();
     organResult = select('#organ');
+    organ_list = '';
     
     adjective2input = select('#poor');
     adj2_value = adjective2input.value();
     adjective2Result = select('#adj2');
+    adj2_list = '';
     
     noun3input = select('#boy');
     noun3_value = noun3input.value();
     noun3Result = select('#noun3');
+    noun3_list = '';
     
     noun4input = select('#wind');
     noun4_value = noun4input.value();
     noun4Result = select('#noun4');
+    noun4_list = '';
     
     familyMemberinput = select('#mama');
     familyMember_value = familyMemberinput.value();
     familyMember1Result = select('#family1');
     familyMember2Result = select('#family2');
     familyMember3Result = select('#family3');
+    family_list = '';
     
     noun5input = select('#gun');
     noun5_value = noun5input.value();
     noun5Result = select('#noun5');
+    noun5_list = '';
     
     bodypartinput = select('#head');
     bodypart_value = bodypartinput.value();
     bodypartResult = select('#bodypart');
+    bodypart_list = '';
     
     verb2input = select('#thrown');
     verb2_value = verb2input.value();
     verb2Result = select('#verb2');
+    verb2_list = '';
     
     carryinput = select('#carry');
     carry_value = carryinput.value();
     carryResult1 = select('#carry1');
     carryResult2 = select('#carry2');
+    carry_list = '';
     
     verb3input = select('#shivers');
     verb3_value = verb3input.value();
     verb3Result = select('#verb3');
+    verb3_list = '';
 
     bodypart2input = select('#spine');
     bodypart2_value = bodypart2input.value();
     bodypart2Result = select('#bodypart2');
+    bodypart2_list = '';
 
     verb4input = select('#leave');
     verb4_value = verb4input.value();
     verb4Result = select('#verb4');
+    verb4_list = '';
     
     noun6input = select('#truth');
     noun6_value = noun6input.value();
     noun6Result = select('#noun6');
-
+    noun6_list = '';
+    
     verb5input = select('#die');
     verb5_value = verb5input.value();
     verb5Result = select('#verb5');
+    verb5_list = '';
     
     adjective3input = select('#little');
     adj3_value = adjective3input.value();
     adjective3Result = select('#adj3');
+    adj3_list = '';
 
     noun7input = select('#silhouetto');
     noun7_value = noun7input.value();
     noun7Result = select('#noun7');
-
+    noun7_list = '';
+    
     nature1input = select('#thunderbolt');
     nature1_value = nature1input.value();
     nature1Result = select('#nature1');
+    nature1_list = '';
 
     nature2input = select('#lightning');
     nature2_value = nature2input.value();
     nature2Result = select('#nature2');
+    nature2_list = '';
     
     humaninput = select('#boy2');
     human_value = humaninput.value();
     humanResult = select('#human');
+    human_list = '';
 
     verb6input = select('#loves');
     verb6_value = verb6input.value();
     verb6Result = select('#verb6');
+    verb6_list = '';
 
     verb7input = select('#spare');
     verb7_value = verb7input.value();
     verb7Result = select('#verb7');
+    verb7_list = '';
 
     noun8input = select('#devil');
     noun8_value = noun8input.value();
     noun8Result = select('#noun8');
+    noun8_list = '';
     
     verb8input = select('#stone');
     verb8_value = verb8input.value();
     verb8Result = select('#verb8');
+    verb8_list = '';
     
     verb9input = select('#spit');
     verb9_value = verb9input.value();
     verb9Result = select('#verb9');
-
+    verb9_list = '';
+    
     verb10input = select('#love');
     verb10_value = verb10input.value();
     verb10Result = select('#verb10');
+    verb10_list = '';
 
     verb11input = select('#leave2');
     verb11_value = verb11input.value();
     verb11Result = select('#verb11');
+    verb11_list = '';
 
     noun9input = select('#wind2');
     noun9_value = noun9input.value();
     noun9Result = select('#noun9');
+    noun9_list = '';
 
     verb12input = select('#blows');
     verb12_value = verb12input.value();
     verb12Result = select('#verb12');
+    verb12_list = '';
 }
 
 function calcWord(input,output){
